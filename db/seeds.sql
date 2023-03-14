6ead8f2329445f1d1f5b09cbd674173d5c24faf4
Jared A <jroberts1931@gmail.com>
2023-03-13 22:07:45 -0400

    added multipe files

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,22 +5,22 @@ VALUES ("Sales"),
        ("Legal");
 
 
-INSERT INTO roles (title, salary)
-VALUES ("Sales Leader", 75,000),
-       ("Salesperson", 60,000),
-       ("Lead Engineer", 150,000),
-       ("Software Engineer", 120,000),
-       ("Account Manager", 125,000),
-       ("Accountant", 100,000),
-       ("Legal Team Lead", 250,000),
-       ("Lawyer", 170,000);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Leader", 75000, 1),
+       ("Salesperson", 60000, 1),
+       ("Lead Engineer", 150000, 2),
+       ("Software Engineer", 120000, 2),
+       ("Account Manager", 125000, 3),
+       ("Accountant", 100000, 3),
+       ("Legal Team Lead", 250000, 4),
+       ("Lawyer", 170000, 4);
 
-INSERT INTO empolyee (first_name, last_name, role_id)
-VALUES ("Moe", "Green"),
-       ("Peter", "Clemenza"),
-       ("Don", "Fanucci"),
-       ("Al", "Neri"),
-       ("Tom", "Hagen"),
-       ("Fredo", "Corleone"),
-       ("Vito", "Corleone"),
-       ("Michael", "Corleone");
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Moe", "Green", 1),
+       ("Peter", "Clemenza", 2),
+       ("Don", "Fanucci", 3),
+       ("Al", "Neri", 4),
+       ("Tom", "Hagen", 5),
+       ("Fredo", "Corleone", 6),
+       ("Vito", "Corleone", 7),
+       ("Michael", "Corleone", 8);
--- server.js
+++ server.js
@@ -9,11 +9,77 @@ const db = mysql.createConnection(
     password: "Monty1020!",
     database: "columbia_db",
     host: "localhost",
-  },
-  console.log("db columbia")
+  }
+  // console.log(database)
 );
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log(`Connected`);
-});
+// const table = "department";
+// db.query(`SELECT * FROM ${table}`, function (err, results, fields) {
+//   console.log(results);
+// });
+
+// db.connect((err) => {
+//   if (err) throw err;
+//   console.log(`Connected`);
+// });
+
+const choices = [
+  {
+    value: "View all departments",
+    num: 1,
+  },
+  {
+    value: "View all roles",
+    num: 2,
+  },
+  {
+    value: "View all employees",
+    num: 3,
+  },
+  {
+    value: "Add a department",
+    num: 4,
+  },
+  {
+    value: "Add a role",
+    num: 5,
+  },
+  {
+    value: "Add an employee",
+    num: 6,
+  },
+  {
+    value: "Update an employee role",
+    num: 7,
+  },
+];
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      choices: choices,
+      name: "beginning",
+    },
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    const table = "department";
+    console.log(department.choices.num);
+    console.log(answers.beginning);
+    db.query(`SELECT * FROM ${table}`, function (err, results, fields) {
+      console.log(results.choices.num);
+      console.log(department.choices.num);
+
+      // if results.num ==1
+    });
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
